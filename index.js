6c120743154d87e918fe348e27479c4540ff65ce
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-08-10 17:59:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function processThis(message, callback) {
+    console.log(message);
+
+}
+document.body.style.backgroundColor="silver";
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,17 @@
+var hopperLab = {
+    name: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.name = " + this.name);
+        console.log("outer func:  self.name = " + self.name);
+        (function() {
+            console.log("inner func:  this.name = " + this.name);
+            console.log("inner func:  self.name = " + self.name);
+        }());
+    }
+};
+hopperLab.func();
+
+// The identifier has been used to to refer to types
+
+
